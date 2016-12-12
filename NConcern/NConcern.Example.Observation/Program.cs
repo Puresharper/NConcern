412bddb19b625124a5cb6ddb3472dc394ba50b05
Tony THONG <tony.thong@outlook.com>
2016-12-12 05:44:46 +0100

    Observation example

---
+++ NConcern/NConcern.Example.Observation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NConcern.Example.Observation
+{
+    public class Observation<T> : IAspect
+        where T : class
+    {
+        public class Argument : EventArgs
+        {
+            public readonly MethodInfo Method;
+
+            public Argument(MethodInfo method)
+            {
+                this.Method = method;
+            }
+        }
+
+        static Observation()
+        {
+            Aspect.Weave<Observation<T>>(_Method => _Method.ReflectedType == Metadata<T>.Type);
+        }
+
+        static public event EventHandler<Argument> Method;
+
+        public IEnumerable<IAdvice> Advise(MethodInfo method)
+        {
+            yield return Advice.Basic.After((_Instance, _Arguments) =>
+            {
+                if (Observation<T>.Method != null) { Observation<T>.Method(_Instance, new Observation<T>.Argument(method)); }
+            });
+        }
+    }
+
+    static public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Attach and observation on all methods of Calculator.
+            Observation<Calculator>.Method += new EventHandler<Observation<Calculator>.Argument>((_Sender, _Argument) => Console.WriteLine(_Argument.Method.Name));
+
+            //Call a method of calculator.
+            var _calculator = new Calculator();
+            _calculator.Add(1, 2);
+        }
+    }
+}
